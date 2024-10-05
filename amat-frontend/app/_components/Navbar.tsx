75b48af5be8cef267df4e2a0ee6b0b046858d458
jessicasewe <jessicasewe89@gmail.com>
2024-10-05 17:44:24 +0000

    feat: login page completed

--- amat-frontend/app/_components/Navbar.tsx
+++ amat-frontend/app/_components/Navbar.tsx
@@ -34,7 +34,7 @@ export default function Navbar() {
         </Link>
       </div>
       <div className="flex items-center space-x-6">
-        <Link href="/login">
+        <Link href="/auth/login">
           <span className="text-blue-900 font-sans font-semibold">Log In</span>
         </Link>
         <Link href="/signup">

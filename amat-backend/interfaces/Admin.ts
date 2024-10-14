ebdf992034d8cfb1023120d6a527685bb2cf5223
jessicasewe <jessicasewe89@gmail.com>
2024-10-14 13:04:26 +0000

    feat: new backend updated

--- amat-backend/interfaces/Admin.ts
+++
@@ -1,9 +0,0 @@
-export interface Admin {
-  name: string;
-  email: string;
-  password: string;
-  phone?: string;
-  address?: string;
-  role: "admin";
-  adminRole: "superadmin" | "manager" | "staff";
-}
